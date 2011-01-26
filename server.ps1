7db40de7ad4487677b3a914ea8ca92c1df272625
mikeplavsky <Mike.Plavsky@quest.com>
2011-01-26 22:19:15 +0300

    Initial commit

---
+++ server.ps1
@@ -0,0 +1,31 @@
+$http = New-Object System.Net.HttpListener
+
+$http.Prefixes.Add( "http://spb9503:81/" )
+$http.Start()
+
+while (1) {
+
+    "Listening...."
+
+    $ctx = $http.GetContext()
+    
+    $res = "Gimme Powershell Please!"  
+    $ctx.Request.RawUrl
+    
+    $query = $ctx.Request.RawUrl -replace "%20", " " -replace "%22", "'"
+    
+    if ( $query -match "^/\?cmd=(.*)" ) {
+        $matches[1]
+        $res = iex $matches[1] | Out-String
+    }
+
+    $buffer = [System.Text.Encoding]::UTF8.GetBytes($res)
+    
+    $ctx.Response.OutputStream.Write( $buffer, 0, $buffer.Length )
+    $ctx.Response.OutputStream.Close()
+    
+    "Done"
+    
+}
+
+

77293bf5221e9de8e3d8ab20b54b565119cd8d7d
Konstantin Vlasenko <konstantin.vlasenko@quest.com>
2011-02-03 14:51:19 +0300

    Rollback to commit [taking argument now]

--- server.ps1
+++ server.ps1
@@ -1,16 +1,7 @@
 $http = New-Object System.Net.HttpListener
 
-if ( !$args.Length ) {
-    "Please, specify url the service will be listening to."
-    return
-}
-
-$error.Clear()
-
 $http.Prefixes.Add( $args[0] )
 $http.Start()
-
-if ($error) { return }
 
 while (1) {
 
@@ -24,35 +15,15 @@ while (1) {
     $query = $ctx.Request.RawUrl -replace "%20", " " -replace "%22", "'"
     
     if ( $query -match "^/\?cmd=(.*)" ) {
-    
+        
         $error.clear()
         
-        $matches[1]        
-        $job = Start-Job {iex "$args" | Out-String} -ArgumentList $matches[1]
-        
-        while ( $job.State -eq "Running" -or $job.State -eq "NotStarted") {
-            Wait-Job $job -Timeout 1            
-        }
-        
-        if ( $job.State -eq "Blocked" ) {
-            $res = "Command requires additional parameters, please, check it out"
-        }
-        else {
-            "Receiving Job"
-            $res = Receive-Job $job
-        }
+        $matches[1]
+        $res = iex $matches[1] | Out-String
         
         if($error) {            
             $res = $error | Out-String            
         }
-
-        "Stopping Job"    
-        
-        $job.StopJob()
-        Get-Job | Remove-Job -Force
-        
-        "Done"
-        
     }
 
     $buffer = [System.Text.Encoding]::UTF8.GetBytes($res)
@@ -61,7 +32,5 @@ while (1) {
     $ctx.Response.OutputStream.Close()
     
     "Done"
-    
+  
 }
-
-
